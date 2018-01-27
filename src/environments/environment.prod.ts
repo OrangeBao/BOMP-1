ad84d8dea815fa4dbd8d24a1e8d2d10dc4eeeebe
baoyinghai <yhbao@amarsoft.com>
2018-01-27 16:07:04 +0800

    add environment notLogin

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,7 @@ declare var GLOBAL_CONFIG: any;
 
 export const environment = {
   production: false,
+  notLogin: false,
   host: (GLOBAL_CONFIG && GLOBAL_CONFIG.host) || '/',
   grafanaHost: (GLOBAL_CONFIG && GLOBAL_CONFIG.grafanaHost) || 'http://10.139.7.119:3000/'
 };
